eaf3a4ebe05bb9ffbd29a7fbce7dff85af335fdb
nomu <tumugi2205@yahoo.co.jp>
2021-09-11 16:36:48 +0900

    オーロクロックに背景追加

--- src/page/OuroClock/OuroClock.js
+++ src/page/OuroClock/OuroClock.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react"
 import './OuroClock.css';
 import Layout from '../../component/Layout/Layout'
+import Header from '../../component/Header/Header'
 
 
 
@@ -16,20 +17,22 @@ const OuroClock = () => {
     var secondDeg = (second * 360 / 60);
     var minuteDeg = (minute * 360 / 60) + ((second * 360 / 60) / 60);
     var hourDeg = (hour * 360 / 12) + ((minute * 360 / 60) / 12)  + ((second * 360 / 60) / 720);
-  
-    console.log(second,secondDeg)
-    console.log(minute,minuteDeg)
-    console.log(hour,hourDeg)
     var animation = document.createElement("style");
     animation.innerHTML = "@keyframes rotation-s {0% {transform-origin:center bottom; transform: rotate(" + secondDeg + "deg)}100% {transform-origin:center bottom; transform: rotate(" + (secondDeg + 360) + "deg)}}@keyframes rotation-m {0% {transform-origin:center bottom; transform: rotate(" + minuteDeg + "deg)}100% {transform-origin:center bottom; transform: rotate(" + (minuteDeg + 360) + "deg)}}@keyframes rotation-h {0% {transform-origin:center bottom; transform: rotate(" + hourDeg + "deg)}100% {transform-origin:center bottom; transform: rotate(" + (hourDeg + 360) + "deg)}}";
     document.head.appendChild(animation);
   }, [])
-  return(
-    <Layout>
+  return(<>
+  <Header/>
+
+<img
+  className="background"
+  src={`./images/background.png`}
+  key="background"/>
+  <div className="ouroclock_area">
     <img
-    className="ouro"
-    src={`./images/Kronii.png`}
-    key="Kronii"/>
+      className="ouro"
+      src={`./images/Kronii.png`}
+      key="Kronii"/>
 
 <img
     className="second"
@@ -45,7 +48,8 @@ const OuroClock = () => {
     className="hour"
     src={`./images/hour.png`}
     key="hour"/>
-    </Layout>
+  </div>
+  </>
   )
 }
 
